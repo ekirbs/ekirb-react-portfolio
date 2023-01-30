002a1c8ff3979f8a34f6cb381789c85d70efb9e1
Eric K <erickirberger@gmail.com>
2023-01-30 15:19:01 -0500

    added init files, made file structure, organizing info

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Card from './components/Card';
+
+// In our main App component, we are rendering only single instances of Header and Navbar and several instances of Card
+function App() {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
+
+
+// import React from "react";
+// import PortfolioContainer from "./components/PortfolioContainer";
+
+// const App = () => <PortfolioContainer />;
+
+// export default App;
