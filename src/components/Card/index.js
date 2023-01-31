cc47a079d52abf1e4b61ab4fdbc65216464d507d
Eric K <erickirberger@gmail.com>
2023-01-31 17:52:07 -0500

    moved style to separate stylesheets

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,32 +1,34 @@
 import React from 'react';
 
+import "./style.css";
+
 // We declare an object called styles that will contain a few objects for card and heading styles
 // Notice that each key lists CSS styles in camel case
-const styles = {
-  card: {
-    margin: 20,
-    background: '#e8eaf6',
-  },
-  heading: {
-    background: '#3f51b5',
-    minHeight: 50,
-    lineHeight: 3.5,
-    fontSize: '1.2rem',
-    color: 'white',
-    padding: '0 20px',
-  },
-  content: {
-    padding: 20,
-  },
-};
+// const styles = {
+//   card: {
+//     margin: 20,
+//     background: '#e8eaf6',
+//   },
+//   heading: {
+//     background: '#3f51b5',
+//     minHeight: 50,
+//     lineHeight: 3.5,
+//     fontSize: '1.2rem',
+//     color: 'white',
+//     padding: '0 20px',
+//   },
+//   content: {
+//     padding: 20,
+//   },
+// };
 
 // In `Card`, we can assign a style from an object by using curly braces
 // We are assigning the card, heading, and content all from our `style` object
 function Card() {
   return (
-    <div style={styles.card}>
-      <div style={styles.heading}>Lorem ipsum dolor</div>
-      <div style={styles.content}>
+    <div className="cardCard">
+      <div className="cardHeading">Lorem ipsum dolor</div>
+      <div className="cardContent">
         `Sed ut perspiciatis unde omnis iste natus error sit voluptatem
         accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
         illo inventore veritatis et quasi architecto beatae vitae dicta sunt
