71d777cdd84cbce4567e0e93d78845d80122ff3b
Eric K <erickirberger@gmail.com>
2023-01-31 19:06:48 -0500

    header has robot image and some styling, moving to navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import thinkingRobot from "../thinkingRobot.jpg";
 
 import './style.css';
 
 function Header() {
   return (
     <header className="header">
+      <img src={thinkingRobot} alt="An image of a robot sitting and thinking."></img>
       <a href="/"><h1>Eric Kirberger || Full Stack Web Developer</h1></a>
     </header>
   );
