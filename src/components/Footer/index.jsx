07c6989277051f038dc1a6024ec4538ca9f6c14a
Eric K <erickirberger@gmail.com>
2023-02-22 14:03:03 -0500

    cleaning up code

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,13 +7,10 @@ import './style.css';
 function Footer() {
   return (
     <footer className="footer">
-      {/* <section className="footer-section"> */}
       <a href="#" target="_blank" rel="noreferrer"><img src={twitterLogo} className="footerZoom rounded-image" alt="Twitter logo."/>Twitter</a>
-        {/* <a href="https://twitter.com/King_of_Florin" target="_blank" rel="noreferrer"><img src={twitterLogo} className="footerZoom rounded-image" alt="Twitter logo."/>Twitter</a> */}
         {/* <a href="mailto:erickirberger@gmail.com">erickirberger@gmail.com</a> */}
         <a href="https://github.com/ekirbs" target="_blank" rel="noreferrer"><img src={githubLogo} className="footerZoom rounded-image" alt="GitHub logo."/>GitHub</a>
         <a href="https://www.linkedin.com/in/eric-kirberger-866a853b/" target="_blank" rel="noreferrer"><img src={linkedinLogo} className="footerZoom rounded-image" alt="Linkedin logo."/>LinkedIn</a>
-      {/* </section> */}
     </footer>
   );
 }
