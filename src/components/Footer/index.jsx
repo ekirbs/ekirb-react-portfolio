20d71c587b5bd49b76a367c1e7081973f5455988
Eric K <erickirberger@gmail.com>
2023-02-22 13:56:34 -0500

    cleaning code, made some edits

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,10 +8,9 @@ function Footer() {
   return (
     <footer className="footer">
       {/* <section className="footer-section"> */}
-        {/* I took out target="_blank" from the <a> links because I was getting terminal warnings...explore */}
-        <a href="https://twitter.com/King_of_Florin" target="_blank" rel="noreferrer"><img src={twitterLogo} className="footerZoom rounded-image" alt="Twitter logo."/>Twitter</a>        
-        {/* <a href="tel:5558675309">(555) 867-5309</a>
-        <a href="mailto:erickirberger@gmail.com">erickirberger@gmail.com</a> */}
+      <a href="#" target="_blank" rel="noreferrer"><img src={twitterLogo} className="footerZoom rounded-image" alt="Twitter logo."/>Twitter</a>
+        {/* <a href="https://twitter.com/King_of_Florin" target="_blank" rel="noreferrer"><img src={twitterLogo} className="footerZoom rounded-image" alt="Twitter logo."/>Twitter</a> */}
+        {/* <a href="mailto:erickirberger@gmail.com">erickirberger@gmail.com</a> */}
         <a href="https://github.com/ekirbs" target="_blank" rel="noreferrer"><img src={githubLogo} className="footerZoom rounded-image" alt="GitHub logo."/>GitHub</a>
         <a href="https://www.linkedin.com/in/eric-kirberger-866a853b/" target="_blank" rel="noreferrer"><img src={linkedinLogo} className="footerZoom rounded-image" alt="Linkedin logo."/>LinkedIn</a>
       {/* </section> */}
