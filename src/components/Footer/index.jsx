e90045279d0942213914f11bd021bb5fde8af282
Eric K <erickirberger@gmail.com>
2023-02-22 14:32:29 -0500

    cleaning

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -10,7 +10,7 @@ function Footer() {
       <a href="#" target="_blank" rel="noreferrer"><img src={twitterLogo} className="footerZoom rounded-image" alt="Twitter logo."/>Twitter</a>
         {/* <a href="mailto:erickirberger@gmail.com">erickirberger@gmail.com</a> */}
         <a href="https://github.com/ekirbs" target="_blank" rel="noreferrer"><img src={githubLogo} className="footerZoom rounded-image" alt="GitHub logo."/>GitHub</a>
-        <a href="https://www.linkedin.com/in/eric-kirberger-866a853b/" target="_blank" rel="noreferrer"><img src={linkedinLogo} className="footerZoom rounded-image" alt="Linkedin logo."/>LinkedIn</a>
+        <a href="https://www.linkedin.com/in/eric-kirberger/" target="_blank" rel="noreferrer"><img src={linkedinLogo} className="footerZoom rounded-image" alt="Linkedin logo."/>LinkedIn</a>
     </footer>
   );
 }
