470ae3c45caa380fa479ca0095bc35e260b8edce
Eric K <erickirberger@gmail.com>
2023-02-22 15:37:42 -0500

    cleaning code, added universal colors

--- src/pages/Portfolio/images/index.js
+++ src/pages/Portfolio/images/index.js
@@ -1,4 +1,3 @@
-// import bostonStreetCuisine from "./images/boston-street-cuisine.png";
 import dailySchedulePlanner from "./daily-sched-screenshot.png";
 import theBigQuiz from "./big-quiz-screenshot.png";
 import natParkTravelGuide from "./nat-park-travel-guide-screenshot.png";
